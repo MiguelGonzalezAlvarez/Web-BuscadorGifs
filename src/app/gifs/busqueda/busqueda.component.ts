030b3437673d8c7925186a704db5ec9256cf4d42
MiguelMuros <miggonzalv@gmail.com>
2021-09-23 20:19:02 +0200

    feat: use viewChild to get search input native element value

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -8,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusquedaComponent implements OnInit {
 
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buscar(): void {
+    this.txtBuscar.nativeElement.value = '';
   }
 
 }
