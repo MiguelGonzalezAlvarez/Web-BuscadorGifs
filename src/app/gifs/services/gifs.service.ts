92e576964ef5ac0b1f45f22d3f1b9743829ff0b9
MiguelMuros <miggonzalv@gmail.com>
2021-09-23 21:42:21 +0200

    feat: show gifs images at results component

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,6 +9,8 @@ export class GifsService {
   private apiKey: string = '1PBkylOhTE1XKtZlU5bA6yRHHFQjDtcM';
   private _historial: string[] = [];
 
+  public resultados: any[] = [];
+
   constructor(private http: HttpClient) { }
 
   get historial() {
@@ -26,9 +28,9 @@ export class GifsService {
     }
 
     // Consumimos la api de giphy para obtener los datos de los gifs
-    this.http.get('api.giphy.com/v1/gifs/search?api_key=1PBkylOhTE1XKtZlU5bA6yRHHFQjDtcM&q=dbz&limit=10')
-      .subscribe((response) => {
-        console.log(response);
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
+      .subscribe((response: any) => {
+        this.resultados = response.data;
       });
 
 
